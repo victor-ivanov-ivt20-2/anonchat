ad1a2d0486d088bf8f6460fbd41f9a93305cb6b4
victor-ivanov-ivt20-2 <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-07-28 04:25:11 +0000

    fix docker-compose

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
-
-const URL = import.meta.env.PROD
+const URL = "ws://45.12.236.183:5000";
+/*const URL = import.meta.env.PROD
   ? import.meta.env.BACKEND_URL
   : "ws://localhost:5000";
+*/
 export const socket = io(URL);
